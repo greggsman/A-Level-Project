9f4363e3bc4a590f0fcfd8b6655f5d6eb833aba3
greggsman <elliotdg10@icloud.com>
2023-11-11 14:33:35 +0000

    Branching

--- Assets/Scripts/ConsumerBehaviour.cs
+++ Assets/Scripts/ConsumerBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using System.Linq;
 
 public class ConsumerBehaviour : MonoBehaviour
 {
@@ -8,11 +9,13 @@ public class ConsumerBehaviour : MonoBehaviour
 
     private SimulationSystemManager ssm;
     private Rigidbody _rigidbody;
+    private List<int> indexesToIgnore;
     int terrainSize;
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         ssm = FindObjectOfType<SimulationSystemManager>();
+        indexesToIgnore = new List<int>();
         foreach (string attributeKey in ConsumerData.attributeKeys)
         {
             stats.attributes[attributeKey] = ssm.simulationSettings[attributeKey];
@@ -21,43 +24,38 @@ public class ConsumerBehaviour : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        if (transform.position.x < 0 || transform.position.x > (terrainSize) * ssm.terrainScale.x ||
+            transform.position.z < 0 || transform.position.z > (terrainSize) * ssm.terrainScale.z)
+        {
+            transform.Rotate(Vector3.up, 180f);
+        }
         Vector3 target = FindTarget(stats.Perceptiveness);
         transform.position += target * stats.Speed * Time.fixedDeltaTime;
         transform.LookAt(transform.position + target);
-
-        if(transform.position.x < -1 || transform.position.x > (terrainSize) * ssm.terrainScale.x ||
-            transform.position.z < -1 || transform.position.z > (terrainSize) * ssm.terrainScale.z)
-        {
-            Debug.Log("out of bounds");
-            transform.Rotate(Vector3.up, 180f);
-        }
     }
     private Vector3 FindTarget(int perceptiveness)
     {
-        Collider[] surroundingObjects = Physics.OverlapSphere(transform.position, perceptiveness, ssm.lm);
-        if(surroundingObjects.Length != 1) // 1 because surroundingObjects arrary still includes this consumer object
+        List<Collider> surroundingObjects = Physics.OverlapSphere(transform.position, perceptiveness, ssm.lm).ToList();
+        if(surroundingObjects.Count != 1) // 1 because surroundingObjects arrary still includes this consumer object
         {
             Vector3 shortestPath = new Vector3(perceptiveness, 100, perceptiveness);
-            foreach (Collider c in surroundingObjects)
+            for(int i = 0; i < surroundingObjects.Count; i++)
             {
-                GameObject surroundingObject = c.gameObject;
+                GameObject surroundingObject = surroundingObjects[i].gameObject;
                 if (surroundingObject.tag == "Consumer")
                 {
                     if (name == surroundingObject.name) continue; // ignore if the organism detected is itself
                     // at the moment testing whether it will run away from other organisms since there is no differentiation between organisms  
                     ConsumerBehaviour nearestConsumerData = surroundingObject.GetComponent<ConsumerBehaviour>();
-                    if (nearestConsumerData.stats.Strength == stats.Strength) // run away
-                    {
-                        return Vector3.Normalize(Quaternion.AngleAxis(180f, Vector3.up) * surroundingObject.transform.position) ; // rotate 180;
-                    }
-                    /*
                     if (nearestConsumerData.stats.Strength > stats.Strength) // run away
                     {
                         return Vector3.Normalize(Quaternion.AngleAxis(180f, Vector3.up) * surroundingObject.transform.position); // rotate 180;
                     }
-                    if (nearestConsumerData.stats.Strength == stats.Strength) continue;
-                    if (nearestConsumerData.stats.Strength < stats.Strength) return Vector3.Normalize(surroundingObject.transform.position - transform.position);
-                    */
+                    if (nearestConsumerData.stats.Strength < stats.Strength)
+                    {
+                        return Vector3.Normalize(surroundingObject.transform.position - transform.position);
+                    }
+                    if (nearestConsumerData.stats.Strength == stats.Strength) break;
                 }
                 else // if its a producer
                 {
@@ -71,10 +69,7 @@ public class ConsumerBehaviour : MonoBehaviour
             shortestPath.y = 0f;
             return Vector3.Normalize(shortestPath);
         }
-        else
-        {
-            return transform.forward;
-        }
+        return transform.forward;
     }
     private void OnCollisionEnter(Collision collision)
     {
